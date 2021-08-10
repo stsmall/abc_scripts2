dc07d8ec60f9bdb2472e98993df0647ab3e348f4
Scott T Small <stsmall@gmail.com>
2021-08-10 11:54:56 -0400

    Update make_coordsfile.py
    added a minor doc

--- utils/make_coordsfile.py
+++ utils/make_coordsfile.py
@@ -3,9 +3,7 @@
 Created on Mon Jan 11 13:50:08 2021
 @author: Scott T. Small
 
-This module demonstrates documentation as specified by the `NumPy
-Documentation HOWTO`_. Docstrings may extend over multiple lines. Sections
-are created with a section header followed by an underline of equal length.
+sims_stats.py obs requires a coords file. 
 
 """
 import sys
